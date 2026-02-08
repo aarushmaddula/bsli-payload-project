4681cb30b700410b8681f01efc4e0e6b8024e5d0
Aarush Maddula <100654110+AarushMaddula@users.noreply.github.com>
2026-02-08 15:39:05 +0000

    Remove IMU data processing functionality

--- main.py
+++ main.py
@@ -1,53 +1,70 @@
 from IMU import IMU
 from datetime import datetime
-import uuid
+
+import numpy as np
+import math
+import csv
+import os
 import time
-from FileManager import FileManager
 
+# PORT = "/dev/pts/5"
 PORT = "/dev/ttyUSB0"
-BAUD = 115200
+BAUD = 230400
 
-arr = ["Time", "Ax", "Ay", "Az", "Roll", "Pitch", "Yaw"]
+now = datetime.now()
 
-log = FileManager(str(uuid.uuid1()) + ".csv", "./data", arr)
+fileName = f"{now.strftime("%m.%d.%Y-%H:%M:%S")}.csv"
+dirName = "./data/"
+
+base_path = os.path.normpath(dirName)
+path = os.path.join(base_path, fileName)
+os.makedirs(base_path, exist_ok=True)
+
+file = open(path, "x")
+w = csv.writer(file)
+ 
+arr = ["Time", "Ax", "Ay", "Az", "Wx", "Wy", "Wz", "q0", "q1", "q2", "q3"]
+
+w.writerow(arr)
+
 imu = IMU(PORT, BAUD)
+imu.openDevice()
 
-while 1:
-    allFrames = imu.getOutputData()
+def quaternionToEulerAngle(q1, q2, q3, q4):
+    
+    a11 = q1 ** 2 - q2 ** 2 - q3 ** 2 + q4 ** 2
+    a12 = 2 * ( q1 * q2 + q3 * q4 )
+    a13 = 2 * ( q1 * q3 - q2 * q4 )
+    a21 = 2 * ( q1 * q2 - q3 * q4 )
+    a22 = q2 ** 2 - q1 ** 2 - q3 ** 2 + q4 ** 2
+    a23 = 2 * ( q2 * q3 + q1 * q4 )
+    a31 = 2 * ( q1 * q3 + q2 * q4 )
+    a32 = 2 * ( q2 * q3 - q1 * q4 )
+    a33 = q3 ** 2 - q1 ** 2 - q2 ** 2 + q4 ** 2
 
-    output = [None] * 8
+    DCM = [	[a11, a12, a13], [a21, a22, a23], [a31, a32, a33] ]
 
-    for frame in allFrames:
-        match frame[1]:
-            case 0x50:
-                hour = frame[5]
-                minute = frame[6]
-                second = frame[7]
+    psi 	= math.atan2( a12, a11 )
+    theta 	= math.atan2( -a13, math.sqrt( a11 * a11 + a12 * a12 ))
+    phi 	= math.atan2( a23, a33 )
 
-                ms = imu.to_int16(frame[8], frame[9])
+    return [phi, theta, psi]
 
-                timeStr = hour * (3600 * 1000) + minute * (60 * 1000) + second * (1000) + ms
-                output[0] = timeStr
+while True:
+    data = imu.getOutputData()
 
-            case 0x51:
-                acc = frame
-
-                ax = imu.to_int16(acc[2], acc[3]) / 32768 * 16
-                ay = imu.to_int16(acc[4], acc[5]) / 32768 * 16
-                az = imu.to_int16(acc[6], acc[7]) / 32768 * 16
-
-                output[1:4] = [ax, ay, az]
-
-            case 0x53:
-                gyro = frame
-
-                roll = imu.to_int16(gyro[2], gyro[3]) / 32768 * 180
-                pitch = imu.to_int16(gyro[4], gyro[5]) / 32768 * 180
-                yaw = imu.to_int16(gyro[6], gyro[7]) / 32768 * 180
-
-                output[4:8] = [ax, ay, az]
-
-    log.recordOutput(output)
-
-
+    w.writerow([
+        data.get("hour", 0) * (3600 * 1000) + data.get("minute", 0) * (60 * 1000) + data.get("second", 0) * (1000) + data.get("millisecond", 0),
+        data.get("ax", "None"),
+        data.get("ay", "None"),
+        data.get("az", "None"),
+        data.get("wx", "None"),
+        data.get("wy", "None"),
+        data.get("wz", "None"),
+        data.get("q0", "None"),
+        data.get("q1", "None"),
+        data.get("q2", "None"),
+        data.get("q3", "None")
+    ])
+ 
     
