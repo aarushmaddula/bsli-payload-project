64fc25b3df6d660b17c8b20a6450f2b874122287
Aarush Maddula <100654110+AarushMaddula@users.noreply.github.com>
2026-03-26 23:53:51 +0000

    Organize Code Segments into Functions

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import math
 import time
+import os
 
 from datetime import datetime
-import dotenv
+from dotenv import load_dotenv
 import numpy as np
 import struct
 import random
@@ -12,64 +13,87 @@ from Servo import Servo
 from LaserVectorCalculator import calculateLaserVector
 from FileManager import FileManager
 
+load_dotenv()
+
 now = datetime.now()
 
-fileName = f"{now.strftime("%m.%d.%Y-%H:%M:%S")}.csv"
+fileName = f"{now.strftime('%m.%d.%Y-%H:%M:%S')}.csv"
 dirName = "./data/"
 headers = ["Time", "Ax", "Ay", "Az", "Wx", "Wy", "Wz", "q0", "q1", "q2", "q3"]
 
 f = FileManager(fileName, dirName, headers)
 
-imu = IMU("/dev/ttyUSB0", 230400)
+imu = IMU(os.getenv("IMU_PORT"), os.getenv("IMU_BAUD"))
 imu.openDevice()
 
-s = Servo("/dev/ttyUSB1", 115200, 1)
+s = Servo(os.getenv("SERVO_PORT"), os.getenv("SERVO_BAUD"), os.getenv("SERVO_1_ID"))
 s.turnMotorOn()
 
-# a = 0
-
-while True:
+def testQuat1():
     data = imu.getOutputData()
 
-    a = int((data.get("yaw", 0) + 180) * 100)
+    d2r = math.pi/180
+    r2d = 180/math.pi
 
-    
-    bites = s.readMultiLoopAngle()    
-    b = struct.unpack('<i', bites[5:9])[0]
+    encoder_z = 0*d2r
+    encoder_a = 0*d2r
 
-    rots = b // 36000
+    encoderData = np.array([encoder_z, encoder_a])
+
+    q0 = data.get("q0", "None")
+    q1 = data.get("q1", "None")
+    q2 = data.get("q2", "None")
+    q3 = data.get("q3", "None")
+
+    quatIMU = np.array([q0, q1, q2, q3])
+
+    e = calculateLaserVector(encoderData, quatIMU)
+
+a = 0
+def testMotor1(a):
+    data = imu.getOutputData()
+
+    # a = int((data.get("yaw", 0) + 180) * 100)
+
+    bites = s.readMultiLoopAngle()
+    b = struct.unpack("<i", bites[5:9])[0]
 
     diff = (b - a + 18000) % 36000 - 18000
     angle = b - diff
+    print(a, "\t", b, "\t", diff, "\t", angle, "\t", bites.hex(" "))
 
-    print(a, "\t", b, "\t", rots,"\t", diff, "\t", angle)
+    s.setMultiLoopAngle(a)
 
-    # s.setSingleLoopAngle(a, 0x01 if diff > 0 else 0x00)
-    s.setMultiLoopAngle(angle)
+    a += int(np.random.normal(loc=0, scale=1000))
+    a %= 36000
 
-    # a += int(np.random.normal(loc=0, scale=1000))
-    # a %= 36000
+    time.sleep(0.1)
 
-    # time.sleep(0.1)
+lastAngle = 0
 
-    # f.recordOutput(data)
-    
-    # d2r = math.pi/180
-    # r2d = 180/math.pi
+try:
+    while True:
+        data = imu.getOutputData()
 
-    # encoder_z = 0*d2r
-    # encoder_a = 0*d2r
+        # a = int((data.get("yaw", 0) + 180) * 100)
 
-    # encoderData = np.array([encoder_z, encoder_a])
+        bites = s.readMultiLoopAngle()
+        b = struct.unpack("<i", bites[5:9])[0]
 
-    # q0 = data.get("q0", "None")
-    # q1 = data.get("q1", "None")
-    # q2 = data.get("q2", "None")
-    # q3 = data.get("q3", "None")
+        diff = (b - a + 18000) % 36000 - 18000
+        angle = b - diff
 
-    # quatIMU = np.array([q0, q1, q2, q3])
-    
-    # e = calculateLaserVector(encoderData, quatIMU)
-    
+        if abs(lastAngle - b) > 36000:
+            print(a, "\t", b, "\t", diff, "\t", angle, "\t", bites.hex(" "))
 
+        lastAngle = b
 
+        s.setMultiLoopAngle(angle)
+
+        a += int(np.random.normal(loc=0, scale=1000))
+        a %= 36000
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    s.turnMotorOff()
