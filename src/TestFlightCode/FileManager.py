ca548ade7adcab2ff5b84bb731a11d6f99ca44e7
Aarush Maddula <100654110+AarushMaddula@users.noreply.github.com>
2026-04-10 03:35:08 +0000

    Improvements to data storage code

--- src/TestFlightCode/FileManager.py
+++ src/TestFlightCode/FileManager.py
@@ -2,26 +2,30 @@ import os
 import csv
 
 class FileManager:
-  def __init__(self, fileName, dirName, headers):
-    base_path = os.path.normpath(dirName)
-    path = os.path.join(base_path, fileName)
-    os.makedirs(base_path, exist_ok=True)
-    file = open(path, "x")
+    def __init__(self, fileName, dirName, headers):
+        base_path = os.path.normpath(dirName)
+        path = os.path.join(base_path, fileName)
+        os.makedirs(base_path, exist_ok=True)
 
-    self.w = csv.writer(file)
-    self.w.writerow(headers)
+        self._file = open(path, "a", newline="")
+        self._w = csv.writer(self._file)
 
-  def recordOutput(self, data):
-    self.w.writerow([
-      data.get("hour", 0) * (3600 * 1000) + data.get("minute", 0) * (60 * 1000) + data.get("second", 0) * (1000) + data.get("millisecond", 0),
-      data.get("ax", "None"),
-      data.get("ay", "None"),
-      data.get("az", "None"),
-      data.get("wx", "None"),
-      data.get("wy", "None"),
-      data.get("wz", "None"),
-      data.get("q0", "None"),
-      data.get("q1", "None"),
-      data.get("q2", "None"),
-      data.get("q3", "None")
-    ])
+        if os.path.getsize(path) == 0:
+            self._w.writerow(headers)
+
+    def recordOutput(self, data):
+        self._w.writerow([
+            data.get("hour", 0) * 3_600_000
+            + data.get("minute", 0) * 60_000
+            + data.get("second", 0) * 1_000
+            + data.get("millisecond", 0),
+            data.get("ax"), data.get("ay"), data.get("az"),
+            data.get("wx"), data.get("wy"), data.get("wz"),
+            data.get("q0"), data.get("q1"), data.get("q2"), data.get("q3"),
+        ])
+        self._file.flush()
+
+    def close(self):
+        self._file.flush()
+        os.fsync(self._file.fileno())
+        self._file.close()
